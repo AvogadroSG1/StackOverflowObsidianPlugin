d9c08f55d86ff3613bb110fd6cc8668680365059
Peter O'Connor <poconnor@BattlesrErasmus.localdomain>
2024-10-13 15:37:55 -0400

    Initial MVP

---
+++ generated-api/apis/index.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AnswersApi';
+export * from './ArticlesApi';
+export * from './CollectionsApi';
+export * from './CommentsApi';
+export * from './CommunitiesApi';
+export * from './QuestionsApi';
+export * from './SearchApi';
+export * from './TagsApi';
+export * from './UserGroupsApi';
+export * from './UsersApi';
---
+++ main.ts
@@ -0,0 +1,379 @@
+import { App, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import { parse, stringify } from 'yaml';
+import { Configuration, ArticlesApi, ArticleResponseModel, TeamsTeamArticlesArticleIdGetRequest, TeamsTeamArticlesArticleIdPutRequest, TeamsTeamArticlesArticleIdLinkedQuestionsGetPageSizeEnum, TeamsTeamArticlesPostRequest } from './generated-api'
+
+/**
+ * Current figuring out why my modal for the getting the Article ID is not working.
+ * 
+ * Start with debugging that.
+ */
+
+interface StackOverflowFBBSyncSettings {
+	PAT: string;
+	teamSlug: string;
+}
+
+const DEFAULT_SETTINGS: StackOverflowFBBSyncSettings = {
+	PAT: 'default',
+	teamSlug: ""
+}
+
+let configuration = new Configuration({
+	basePath: 'https://api.stackoverflowteams.com/v3',
+	headers: {
+		'accept': 'application/json',
+		'Authorization': 'Bearer ' + ''
+	}
+});
+
+const defaultTagIfNonePresent = ['obsidian'];
+
+export class DisplayMessageModal extends Modal {
+	constructor(app: App, message: string) {
+		super(app);
+		this.setContent(`${message}`)
+	}
+}
+
+export class GetArticleIdModal extends Modal {
+	constructor(
+		app: App, onSubmit: (result: string) => void) {
+		super(app);
+		this.setTitle("What is the Id of the article you'd like to retrieve?");
+
+		let articleId = '';
+		new Setting(this.contentEl)
+			.setName("Article Id")
+			.addText((text) =>
+				text.onChange((value) => {
+					articleId = value;
+				}));
+
+		new Setting(this.contentEl)
+			.addButton((btn) =>
+				btn
+					.setButtonText("Submit")
+					.setCta()
+					.onClick(() => {
+						this.close();
+						onSubmit(articleId);
+					}));
+	}
+}
+
+export default class StackOverflowFBBSync extends Plugin {
+	//this is for the settings tab
+	settings: StackOverflowFBBSyncSettings = DEFAULT_SETTINGS;
+
+	apiClient = new ArticlesApi(configuration);
+
+	async onload() {
+		await this.loadSettings();
+
+		// This creates an icon in the left ribbon.
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Stack Overflow Teams', (evt: MouseEvent) => {
+			// Called when the user clicks the icon.
+			new Notice('Sync with FBB');
+		});
+		// Perform additional things with the ribbon
+		ribbonIconEl.addClass('my-plugin-ribbon-class');
+
+		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+		const statusBarItemEl = this.addStatusBarItem();
+		statusBarItemEl.setText('Stack Overflow for Teams');
+
+		// This adds a complex command that can check whether the current state of the app allows execution of the command
+		this.addCommand({
+			id: 'get-from-so-teams-fbb',
+			name: 'Get Article From Stack Overflow for Teams',
+			callback: () => {
+				new GetArticleIdModal(this.app, (articleId: string) => {
+					this.getArticleByID(articleId);
+				}).open();
+			}
+		});
+
+		this.addCommand({
+			id: 'sync-to-so-teams-fbb',
+			name: 'Save Article From Stack Overflow for Teams',
+			checkCallback: (checking: boolean) => {
+				// Conditions to check
+				if (this.app.workspace.getActiveViewOfType(MarkdownView)) {
+					if (!checking) {
+						this.saveArticle();
+					}
+					// This command will only show up in Command Palette when the check function returns true
+					return true;
+				}
+				return false;
+			}
+		});
+
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new StackOverflowFBBSyncSettingsTab(this.app, this));
+
+		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+		// Using this function will automatically remove the event listener when this plugin is disabled.
+		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+			console.log('click', evt);
+		});
+
+		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	}
+
+	async onunload() {
+
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		await this.createArticlesApi();
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+		await this.createArticlesApi();
+	}
+
+	/**
+	 * This function is used to create the Articles API client.
+	 * 
+	 * @returns {Promise<void>}
+	 * @memberof StackOverflowFBBSync
+	 * @since 0.0.1
+	 * @version 0.0.1
+	 * 
+	 * TODO: Determine why 'Authorization' is not being set in the headers from the generated API client.
+	 */
+	private async createArticlesApi(): Promise<void> {
+		this.apiClient = new ArticlesApi(new Configuration({
+			basePath: 'https://api.stackoverflowteams.com/v3',
+			headers: {
+				'accept': 'application/json',
+				'Authorization': 'Bearer ' + this.settings.PAT
+			}
+		}));
+	}
+
+	getArticleByID(articleId: string): Promise<void | ArticleResponseModel> {
+		const requestParameters = { articleId: +articleId, team: this.settings.teamSlug } as TeamsTeamArticlesArticleIdGetRequest;
+		return this.apiClient.teamsTeamArticlesArticleIdGet(requestParameters)
+			.then(async (article: ArticleResponseModel) => {
+				if (article) {
+					const articleFolderName = `${this.settings.teamSlug}`;
+					const articleFileName = `${articleFolderName}/${article.title} - ${article.id}.md`;
+
+					this.app.vault.adapter.exists(articleFolderName)
+						.then((subFolderExists: boolean) => {
+							if (!subFolderExists) {
+								this.app.vault.createFolder(articleFolderName)
+							}
+						}).then(() => {
+							this.app.vault.adapter.exists(articleFileName).then((exists: boolean) => {
+								{
+									if (!exists) {
+										this.app.vault.create(articleFileName, '').then((newFile: TFile) => {
+											this.app.workspace.getLeaf().openFile(newFile).then(() => {
+												// TODO: Change this to a small notification
+												console.log("Switched to the new file:", newFile.path);
+
+												//Now fill in the document with the article content
+												this.populateArticleFromArticleResponseModel(newFile, article);
+											}).catch((err) => {
+												console.error("Error switching to the new file:", err);
+											});
+										}).catch((err) => {
+											new DisplayMessageModal(this.app, `Error creating file: ${err}`);
+										});
+									} else {
+										let existingFile = this.app.vault.getFileByPath(articleFileName)!;
+
+										this.app.workspace.getLeaf().openFile(existingFile).then(() => {
+											// TODO: Change this to a small notification
+											console.log("Switched to the new file:", existingFile.path);
+
+											//Now fill in the document with the article content
+											this.populateArticleFromArticleResponseModel(existingFile, article);
+										}).catch((err) => {
+											console.error("Error switching to the new file:", err);
+										});
+									}
+								};
+							});
+						})
+				}
+				else {
+					new Notice('Article not found');
+				}
+			}
+			);
+	}
+
+	async saveArticle(): Promise<ArticleResponseModel | boolean | void> {
+		const activeFile = this.app.workspace.getActiveFile();
+		if (!activeFile) {
+			new Notice('No active file found');
+			return Promise.resolve(false);
+		}
+
+		return this.app.vault.cachedRead(activeFile)
+			.then((content: string) => {
+				let fileContent = content;
+
+				// Extract frontmatter block (YAML block) if exists
+				const frontmatterRegex = /^---\n([\s\S]*?)\n---/;
+				const frontmatterMatch = fileContent.match(frontmatterRegex);
+
+				let frontmatter: any = {};
+				let remainderOfContent = fileContent.replace(frontmatterRegex, '').trim();;
+				if (frontmatterMatch) {
+					// Parse the existing frontmatter
+					frontmatter = parse(frontmatterMatch[1]);
+				}
+
+				if (frontmatter.articleId) {
+					const updadateModel = {
+						articleId: frontmatter.articleId,
+						team: this.settings.teamSlug,
+						articleRequestModel: {
+							title: frontmatter.title,
+							body: remainderOfContent,
+							bodyMarkdown: remainderOfContent,
+							tags: frontmatter.tags,
+							type: frontmatter.type,
+							permissions: frontmatter.permissions
+						}
+					} as TeamsTeamArticlesArticleIdPutRequest;
+
+					return this.updateArticle(updadateModel);
+				}
+				else {
+					const createModel = {
+						team: this.settings.teamSlug,
+						articleRequestModel: {
+							title: frontmatter.title ? frontmatter.title : activeFile.name.substring(0, activeFile.name.indexOf('.')),
+							body: remainderOfContent,
+							bodyMarkdown: remainderOfContent,
+							tags: frontmatter.tags ?? defaultTagIfNonePresent,
+							type: frontmatter.type ? frontmatter.type : 'knowledgeArticle',
+							permissions: frontmatter.permissions
+						}
+					} as TeamsTeamArticlesPostRequest;
+
+					return this.createArticle(createModel)
+						.then((response) => {
+							if (response) {
+								response.bodyMarkdown ??= remainderOfContent.trim();
+								this.populateArticleFromArticleResponseModel(activeFile, response as ArticleResponseModel).then(() => {
+									activeFile.vault.rename(activeFile, `${this.settings.teamSlug}/${response.title} - ${response.id}.md`);
+								});
+							}
+						}
+						);
+				}
+			});
+	}
+
+	private async updateArticle(article: TeamsTeamArticlesArticleIdPutRequest) {
+		return this.apiClient.teamsTeamArticlesArticleIdPut(article)
+			.then((response) => {
+				const message = `${response.title} updated`;
+				new DisplayMessageModal(this.app, message).open();
+				return true;
+			})
+			.catch((error: any) => {
+				// Handle the error if needed
+				console.error('Error saving article:', error);
+				return false;
+			});
+	}
+
+	private async createArticle(article: TeamsTeamArticlesPostRequest) {
+		return this.apiClient.teamsTeamArticlesPost(article)
+			.then((response) => {
+				const message = `${response.title} created`;
+				new DisplayMessageModal(this.app, message).open();
+				return response;
+			})
+			.catch((error: any) => {
+				// Handle the error if needed
+				console.error('Error saving article:', error);
+				return null;
+			});
+	}
+
+	private async populateArticleFromArticleResponseModel(activeFile: TFile, article: ArticleResponseModel): Promise<void> {
+		let frontmatter: any = {};
+
+		// Add or update the property
+		frontmatter['articleId'] = article.id;
+		frontmatter['tags'] = article.tags;
+		frontmatter['communities'] = article.communities?.map(community => community.name);
+		frontmatter['id'] = article.id;
+		frontmatter['type'] = article.type;
+		frontmatter['title'] = article.title;
+		frontmatter['tags'] = article.tags?.map(tag => tag.name);
+		frontmatter['owner'] = article.owner?.name;
+		frontmatter['lastEditor'] = article.lastEditor?.name;
+		frontmatter['creationDate'] = article.creationDate;
+		frontmatter['lastActivityDate'] = article.lastActivityDate;
+		frontmatter['score'] = article.score;
+		frontmatter['viewCount'] = article.viewCount;
+		frontmatter['shareUrl'] = article.shareUrl;
+		frontmatter['isDeleted'] = article.isDeleted;
+		frontmatter['isObsolete'] = article.isObsolete;
+		frontmatter['isClosed'] = article.isClosed;
+		frontmatter['userIsFollowing'] = article.userIsFollowing;
+		frontmatter['userHasUpvoted'] = article.userHasUpvoted;
+		frontmatter['userHasDownvoted'] = article.userHasDownvoted;
+		frontmatter['userCanEdit'] = article.userCanEdit;
+		frontmatter['permissions'] = article.permissions;
+
+		// Convert the updated frontmatter back to YAML
+		const updatedFrontmatter = stringify(frontmatter);
+
+		// Rebuild the file content
+		const newContent = `---\n${updatedFrontmatter}---\n\n${article.bodyMarkdown}`;
+
+		// Write the updated content back to the file
+		this.app.vault.modify(activeFile, newContent);
+	}
+}
+
+class StackOverflowFBBSyncSettingsTab extends PluginSettingTab {
+	plugin: StackOverflowFBBSync;
+
+	constructor(app: App, plugin: StackOverflowFBBSync) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('API Key')
+			.setDesc('Generate this token from \"Account Settings\"--> Personal Access Tokens')
+			.addText(text => text
+				.setPlaceholder('Enter your secret')
+				.setValue(this.plugin.settings.PAT)
+				.onChange(async (value) => {
+					this.plugin.settings.PAT = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Team Slug')
+			.setDesc('Find this in the URL of your team page')
+			.addText(text => text
+				.setPlaceholder('Enter your Team Slug')
+				.setValue(String(this.plugin.settings.teamSlug))
+				.onChange(async (value) => {
+					this.plugin.settings.teamSlug = value;
+					await this.plugin.saveSettings();
+				}));
+
+	}
+}
