8f29e923b3187a302052bbccc39f52c592dbb74e
Peter O'Connor <avogadrosg1+soteamsplugin@gmail.com>
2024-11-04 20:09:30 -0500

    Updated the type check

--- generated-api/models/ArticleRequestModel.ts
+++ generated-api/models/ArticleRequestModel.ts
@@ -70,7 +70,7 @@ export interface ArticleRequestModel {
  * Check if a given object implements the ArticleRequestModel interface.
  */
 export function instanceOfArticleRequestModel(value: object): value is ArticleRequestModel {
-    if (!('title' in value) || value.title === undefined) return false;
+    if (!('title' in value) || (('title' in value) && value.title === undefined)) return false;
     if (!('body' in value) || value.body === undefined) return false;
     if (!('tags' in value) || value.tags === undefined) return false;
     if (!('type' in value) || value.type === undefined) return false;
